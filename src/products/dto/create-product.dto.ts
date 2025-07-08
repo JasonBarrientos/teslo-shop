20f5774897113ad9ee3f3e38d55afd4dc88acc20
jasonBarrientos <jasonbarrientos.dev@gmail.com>
2025-07-08 07:10:50 -0300

    se crea dto de crete products

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,31 @@
-export class CreateProductDto {}
+import { IsArray, IsIn, IsInt, IsNumber, IsOptional, IsPositive, isString, IsString, MinLength } from "class-validator";
+
+export class CreateProductDto {
+
+    @IsString()
+    @MinLength(1)
+    title: string;
+
+    @IsNumber()
+    @IsPositive()
+    @IsOptional()
+    price?: number;
+
+    @IsString()
+    @IsOptional()
+    description?: string;
+    @IsString()
+    @IsOptional()
+    slug?: string;
+
+    @IsInt()
+    @IsOptional()
+    stock?: number;
+
+    @IsArray()
+    @IsString({each:true})
+    sizes: string[];
+
+    @IsIn(['men','women','kid','unisex'])
+    gender: string;
+}
