30b114dd7343055ade3e40436072a7bd519b6139
jasonBarrientos <jasonbarrientos.dev@gmail.com>
2025-07-21 07:10:06 -0300

    1er parte de socket.io

---
+++ src/messages-ws/messages-ws.gateway.ts
@@ -0,0 +1,16 @@
+import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from '@nestjs/websockets';
+import { MessagesWsService } from './messages-ws.service';
+import { Socket } from 'socket.io';
+
+@WebSocketGateway({cors:true })
+export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect{
+  constructor(private readonly messagesWsService: MessagesWsService) {}
+  handleDisconnect(client: Socket) {
+    console.log(`Cliente conectado ${client.id}`);
+    
+  }
+  handleConnection(client: Socket, ...args: any[]) {
+        console.log(`Cliente conectado ${client.id}`);
+
+  }
+}
